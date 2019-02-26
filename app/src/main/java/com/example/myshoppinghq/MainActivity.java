163693b78d888d2bdb15fab2308e11323d670ec7
cur1576@gmx.de <8sje7wksksK9wure>
2019-02-26 11:53:40 +0100

    commit_26.02_03

--- app/src/main/java/com/example/myshoppinghq/MainActivity.java
+++ app/src/main/java/com/example/myshoppinghq/MainActivity.java
@@ -1,13 +1,17 @@
 package com.example.myshoppinghq;
 
+import android.content.DialogInterface;
 import android.os.Bundle;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.text.TextUtils;
 import android.util.Log;
 import android.util.SparseBooleanArray;
 import android.view.ActionMode;
+import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 import android.view.inputmethod.InputMethodManager;
 import android.widget.AbsListView;
 import android.widget.ArrayAdapter;
@@ -24,7 +28,7 @@ public class MainActivity extends AppCompatActivity {
 
     private static final String TAG = MainActivity.class.getSimpleName();
     ShoppingMemoDataSource dataSource;
-    private  boolean isButtonClick = true;
+    private boolean isButtonClick = true;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -38,22 +42,21 @@ public class MainActivity extends AppCompatActivity {
     }
 
 
-
     private void activateAddButton() {
 
         final EditText editTextQuantity = findViewById(R.id.editText_quantity);
         final EditText editTextProduct = findViewById(R.id.editText_product);
         Button buttonAddProduct = findViewById(R.id.button_add_prduct);
-        buttonAddProduct.setOnClickListener(v->{
+        buttonAddProduct.setOnClickListener(v -> {
 
             String quantityString = editTextQuantity.getText().toString();
             String product = editTextProduct.getText().toString();
 
-            if(TextUtils.isEmpty(quantityString)){
+            if (TextUtils.isEmpty(quantityString)) {
                 editTextQuantity.setError(getString(R.string.editText_errorMessage));
                 return;
             }
-            if(TextUtils.isEmpty(product)){
+            if (TextUtils.isEmpty(product)) {
                 editTextProduct.setError(getString(R.string.editText_errorMessage));
                 return;
             }
@@ -62,21 +65,21 @@ public class MainActivity extends AppCompatActivity {
             editTextProduct.setText("");
             editTextQuantity.setText("");
 
-            dataSource.createShoppingMemo(product,quantity);
+            dataSource.createShoppingMemo(product, quantity);
 
             InputMethodManager inputMethodManager = (InputMethodManager) getSystemService(INPUT_METHOD_SERVICE);
-            if(getCurrentFocus()!=null && isButtonClick){
-                inputMethodManager.hideSoftInputFromWindow(getCurrentFocus().getWindowToken(),0);
+            if (getCurrentFocus() != null && isButtonClick) {
+                inputMethodManager.hideSoftInputFromWindow(getCurrentFocus().getWindowToken(), 0);
 
             }
 
             showAllListEntries();
         });
-        editTextProduct.setOnEditorActionListener((textView,pos,keyEvent)->{
-            isButtonClick=false;
+        editTextProduct.setOnEditorActionListener((textView, pos, keyEvent) -> {
+            isButtonClick = false;
             buttonAddProduct.performClick();
             editTextQuantity.requestFocus();
-            isButtonClick=true;
+            isButtonClick = true;
             return true;
         });
     }
@@ -94,7 +97,7 @@ public class MainActivity extends AppCompatActivity {
 
             @Override
             public boolean onCreateActionMode(ActionMode mode, Menu menu) {
-                getMenuInflater().inflate(R.menu.menu_contextual_action_bar,menu);
+                getMenuInflater().inflate(R.menu.menu_contextual_action_bar, menu);
                 return true;
             }
 
@@ -105,12 +108,12 @@ public class MainActivity extends AppCompatActivity {
 
             @Override
             public boolean onActionItemClicked(ActionMode mode, MenuItem item) {
-                switch (item.getItemId()){
+                switch (item.getItemId()) {
                     case R.id.cab_delete:
                         SparseBooleanArray touchedShoppingMemosPosition = shoppingMemoListView.getCheckedItemPositions();
-                        for(int i=0; i<touchedShoppingMemosPosition.size(); i++){
+                        for (int i = 0; i < touchedShoppingMemosPosition.size(); i++) {
                             boolean isChecked = touchedShoppingMemosPosition.valueAt(i);
-                            if(isChecked){
+                            if (isChecked) {
                                 int positionInListView = touchedShoppingMemosPosition.keyAt(i);
                                 ShoppingMemo shoppingMemo =
                                         (ShoppingMemo) shoppingMemoListView.getItemAtPosition(positionInListView);
@@ -133,6 +136,51 @@ public class MainActivity extends AppCompatActivity {
         });
     }
 
+    private AlertDialog CreateShoppingMemoDialog(final ShoppingMemo shoppingMemo) {
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        LayoutInflater inflater = getLayoutInflater();
+
+        View dialogsView = inflater.inflate(R.layout.dialog_edit_shopping_memo, null);
+
+        final EditText editTextNewQuantity = dialogsView.findViewById(R.id.editText_new_quantity);
+        editTextNewQuantity.setText(shoppingMemo.getQuantity());
+
+        final EditText editTextNewProduct = dialogsView.findViewById(R.id.editText_new_product);
+        editTextNewProduct.setText(shoppingMemo.getProduct());
+
+        builder.setView(dialogsView)
+                .setTitle(R.string.dialog_titel)
+                .setPositiveButton(R.string.dialog_button_positiv, new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        String quantityString = editTextNewQuantity.getText().toString();
+                        String product = editTextNewProduct.getText().toString();
+
+                        if (TextUtils.isEmpty(quantityString) || TextUtils.isEmpty(product)) {
+                            Toast.makeText(MainActivity.this, "Felder dürfen nicht leer sein", Toast.LENGTH_SHORT).show();
+                            return;
+                        }
+                        int quantity = Integer.parseInt(quantityString);
+
+                        ShoppingMemo memo = dataSource.updateShoppingMemo(shoppingMemo.getId(), product, quantity);
+
+                        Log.d(TAG, "Alter Eintrag - ID: " + shoppingMemo.getId() + " Inhalt: " + shoppingMemo.toString());
+                        Log.d(TAG, "Neuer Eintrag - ID: " + memo.getId() + " Inhalt: " + memo.toString());
+
+                        showAllListEntries();
+                        dialog.dismiss();
+                    }
+                })
+                .setNegativeButton(R.string.dialog_button_negativ, new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        dialog.cancel();
+                    }
+                });
+        return builder.create();
+
+    }
+
     @Override
     protected void onResume() {
         super.onResume();
@@ -152,7 +200,7 @@ public class MainActivity extends AppCompatActivity {
         List<ShoppingMemo> shoppingMemos = dataSource.getAllShoppingMemos();
 
         ArrayAdapter<ShoppingMemo> shoppingMemoArrayAdapter = new ArrayAdapter<>(this,
-                android.R.layout.simple_list_item_multiple_choice,shoppingMemos);
+                android.R.layout.simple_list_item_multiple_choice, shoppingMemos);
 
         ListView shoppingMemoListView = findViewById(R.id.listview_shopping_memos);
         shoppingMemoListView.setAdapter(shoppingMemoArrayAdapter);
@@ -162,13 +210,13 @@ public class MainActivity extends AppCompatActivity {
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
-        getMenuInflater().inflate(R.menu.menu_main,menu);
+        getMenuInflater().inflate(R.menu.menu_main, menu);
         return true;
     }
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
-        switch(item.getItemId()){
+        switch (item.getItemId()) {
             case R.id.action_settings:
                 Toast.makeText(this, "Settings wurde gedrückt", Toast.LENGTH_SHORT).show();
                 return true;
